ed7d50f85e9e85edc01c7e9e09829766171627a8
Wagura Maurice <business@waguramaurice.com>
2024-05-12 19:06:31 +0300

    feat: create MainActivity.java

---
+++ app/src/main/java/com/waguramaurice/droid/MainActivity.java
@@ -0,0 +1,15 @@
+// app/src/main/java/com/waguramaurice/droid/MainActivity.java
+
+package com.waguramaurice.droid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
